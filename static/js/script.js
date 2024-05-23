8b5ce43372a97ee303cb6f7f9e13c396ac472eac
yoavlevi6 <yoavlevi0606@gmail.com>
2024-05-23 14:41:34 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-    $('#motorSlider').on('input', function() {
-        let speed = $(this).val();
-        $('#motorSpeedValue').text(speed);
-        
-        $.ajax({
-            url: '/update_motor_speed',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({speed: speed}),
-            success: function(response) {
-                console.log('Motor speed updated:', response.motor_speed);
-            }
+function updateCircle() {
+    fetch('/status')
+        .then(response => response.json())
+        .then(data => {
+            const circle = document.getElementById('statusCircle');
+            circle.style.backgroundColor = data.status;
         });
-    });
-});
+}
+
+// Check status every second
+setInterval(updateCircle, 1000);
+
+// Initial check
+updateCircle();
