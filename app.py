b3b691afdef49114096461e79a54e32aba06bdf7
yoavlevi6 <yoavlevi0606@gmail.com>
2024-05-28 21:00:53 +0300

    1

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 import time
 
 app = Flask(__name__)
-
-# משתנה לאחסון הזמן של העדכון האחרון
 last_update_time = 0
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/update', methods=['POST'])
-def update():
-    global last_update_time
-    last_update_time = time.time()
-    return jsonify(success=True)
 
 @app.route('/status', methods=['GET'])
 def status():
@@ -22,7 +14,14 @@ def status():
     current_time = time.time()
     if current_time - last_update_time < 10:
         return jsonify(status='green')
-    return jsonify(status='red')
+    else:
+        return jsonify(status='red')
+
+@app.route('/update', methods=['POST'])
+def update():
+    global last_update_time
+    last_update_time = time.time()
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
