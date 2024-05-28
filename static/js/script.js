5f9438c81e0f037ac6d89548cbef1ba3adaf8178
yoavlevi6 <yoavlevi0606@gmail.com>
2024-05-28 21:01:49 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,20 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    fetch('/update', { method: 'POST' });
+
+    var circle = document.getElementById('statusCircle');
+    circle.style.backgroundColor = 'green';
+
+    setTimeout(function() {
+        circle.style.backgroundColor = 'red';
+    }, 10000); // 10 seconds
+});
+
 function updateCircle() {
     fetch('/status')
         .then(response => response.json())
         .then(data => {
             const circle = document.getElementById('statusCircle');
-            circle.style.backgroundColor = data.status;
+            circle.style.backgroundColor = data.status === 'green' ? 'green' : 'red';
         });
 }
 
