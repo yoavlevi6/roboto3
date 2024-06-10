2ecefce25e43e5aa8618428cb1147661816126d1
yoavlevi6 <yoavlevi0606@gmail.com>
2024-06-10 10:48:26 +0300

    app

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
-from flask import Flask, Response, send_from_directory
+from flask import Flask, render_template, jsonify
 import time
-import os
 
 app = Flask(__name__)
 
-# פונקציה זו מפיקה אירועים לחיישן מגנט
-def magnet_sensor_events():
-    while True:
-        magnet_detected = True if time.time() % 5 < 2 else False
-        yield f"data: {'detected' if magnet_detected else 'not detected'}\n\n"
-        time.sleep(1)
-
-# פונקציה זו מפיקה אירועים לחיישן אולטרהסוניק
-def ultrasonic_sensor_events():
-    while True:
-        distance = 20 if time.time() % 7 < 2 else 30
-        yield f"data: {distance}\n\n"
-        time.sleep(1)
+# משתנה לאחסון הזמן של העדכון האחרון
+last_update_time = 0
 
 @app.route('/')
 def index():
-    return send_from_directory(os.getcwd(), 'index.html')
+    return render_template('index.html')
 
-@app.route('/magnet_events', methods=['GET'])
-def magnet_events():
-    return Response(magnet_sensor_events(), content_type='text/event-stream')
+@app.route('/update', methods=['POST'])
+def update():
+    global last_update_time
+    last_update_time = time.time()
+    return jsonify(success=True)
 
-@app.route('/ultrasonic_events', methods=['GET'])
-def ultrasonic_events():
-    return Response(ultrasonic_sensor_events(), content_type='text/event-stream')
+@app.route('/status', methods=['GET'])
+def status():
+    global last_update_time
+    current_time = time.time()
+    if current_time - last_update_time < 10:
+        return jsonify(status='green')
+    return jsonify(status='red')
 
 if __name__ == '__main__':
     app.run(debug=True)
