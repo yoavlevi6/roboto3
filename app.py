7876cf83ab0bf3ecb32a1a18d5b61df4f5cfdc68
yoavlevi6 <yoavlevi0606@gmail.com>
2024-06-10 11:22:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
-from flask import Flask, Response, request, send_from_directory
+from flask import Flask, Response, send_from_directory
 import time
 import os
 
 app = Flask(__name__)
 
-ultrasonic_status = 100  # initialize with a value greater than 25
+# פונקציה זו מפיקה אירועים לחיישן מגנט
+def magnet_sensor_events():
+    while True:
+        magnet_detected = True if time.time() % 5 < 2 else False
+        yield f"data: {'detected' if magnet_detected else 'not detected'}\n\n"
+        time.sleep(1)
 
+# פונקציה זו מפיקה אירועים לחיישן אולטרהסוניק
 def ultrasonic_sensor_events():
     while True:
-        yield f"data: {ultrasonic_status}\n\n"
+        distance = 20 if time.time() % 7 < 2 else 30
+        yield f"data: {distance}\n\n"
         time.sleep(1)
 
 @app.route('/')
 def index():
     return send_from_directory(os.getcwd(), 'index.html')
 
-@app.route('/script.js')
-def script():
-    return send_from_directory(os.getcwd(), 'script.js')
+@app.route('/magnet_events', methods=['GET'])
+def magnet_events():
+    return Response(magnet_sensor_events(), content_type='text/event-stream')
 
 @app.route('/ultrasonic_events', methods=['GET'])
 def ultrasonic_events():
     return Response(ultrasonic_sensor_events(), content_type='text/event-stream')
-
-@app.route('/update_ultrasonic_status', methods=['GET'])
-def update_ultrasonic_status():
-    global ultrasonic_status
-    ultrasonic_status = request.args.get('status')
-    print(f"Received status: {ultrasonic_status}")  # הוסף את השורה הזו לאבחון
-    return "Status received", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
